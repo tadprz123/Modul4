70241bf5e963023b2e90705806c9df6dd36d9f74
tadprz123 <przybylski.tadeusz@gmail.com>
2024-07-22 11:07:05 +0200

    Update Imię_i_nazwisko.py

--- Imię_i_nazwisko.py
+++ Imię_i_nazwisko.py
@@ -6,18 +6,4 @@ if __name__ == "__main__":
     done = name_surname_text
     name_surname_result = name_surname(done)
     print(name_surname_result)
-"""
-def shopping(items, payment='card', shop='local'):
-    result = ""
-    result = result + "Idę na zakupy do %s.\n" % shop
-    result = result + "Kupię następujące rzeczy:\n"
-    for item in items:
-        result = result + " - %s\n" % item
-    result = result + "By zapłacić, używam %s." % payment
-    return result
-if __name__ == "__main__":
-    items_text = input("Podaj produkty rozdzielone przecinkiem: ")
-    items = items_text.split(', ')
-    shopping_result = shopping(items)
-    print(shopping_result)
-"""
+
