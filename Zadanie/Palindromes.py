0109c4f6c3b7b3bf6b69c155179bda19a4edc201
Tadeusz Przybylski <przybylski.tadeusz@gmail.com>
2024-07-22 11:04:36 +0200

    kalkulator.py

--- Zadanie/Palindromes.py
+++ Zadanie/Palindromes.py
@@ -1,5 +1,6 @@
 def palindromes(str):
     #funkcja porównuje wyraz od przodu i od tyłu
+    #wielkie litery mają znaczenie w porównywaniu, stąd str.lower()
     str = str.lower()
     from_end = str[::-1]
     if str == from_end:
